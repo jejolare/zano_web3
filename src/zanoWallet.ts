b9cab10b637142fc78b7144d7e20c151f306e61d
jejolare <jejolare@gmail.com>
2024-07-24 18:49:03 +0700

    add hooks

--- src/zanoWallet.ts
+++ src/zanoWallet.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-interface ZanoWalletParams {
+export interface ZanoWalletParams {
     authPath: string;
     useLocalStorage?: boolean; // default: true
     aliasRequired?: boolean; 
